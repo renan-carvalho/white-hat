6e06da6a611b4ec0857586713235221327395c21
Renan Carvalho <recarvalhodesouza@gmail.com>
2023-04-02 17:17:06 -0300

    removido o tf que formatava o resultado do ping

--- ipsweep.sh
+++ ipsweep.sh
@@ -7,6 +7,6 @@ echo "Exemplo: ./ipsweep 192.168.1"
 
 else
 for ip in $(seq 1 254); do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tf -d ":" &
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 &
 done
 fi
