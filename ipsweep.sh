4a18dd44c93f80bb171846ccc0409369d989dc0f
Renan Carvalho <recarvalhodesouza@gmail.com>
2023-04-02 17:08:06 -0300

    mecanica do ping sweep

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+if ["$1" == 0]
+then
+echo "Você esqueceu o endereço de IP!"
+echo "Exemplo: ./ipsweep 192.168.1"
+
+else
+for ip in $(seq 1 254); do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tf -d ":" &
+done
+fi
